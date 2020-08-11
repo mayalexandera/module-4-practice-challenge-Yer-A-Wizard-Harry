9a13a1a3f79baa535c39280f6697ad38029a4967
mayalexandera <maya.blevins@gmail.com>
2020-08-10 23:16:51 -0400

    finish

--- src/App.js
+++ src/App.js
@@ -4,26 +4,23 @@ import StorySettings from "./StorySettings";
 import WizardList from "./WizardList";
 import "./App.css";
 
-class App extends Component {
+const wizzardsUrl = `http://hp-api.herokuapp.com/api/characters/students`;
 
+class App extends Component {
   state = {
     students: [],
     filter: 'All'
   }
   componentDidMount() {
-    fetch(`http://hp-api.herokuapp.com/api/characters/students`)
-    .then(resp=>resp.json())
+    fetch(wizzardsUrl).then(resp=>resp.json())
     .then(data=> this.setState({students: data}))
   } 
  
-  addStudent = ({name, house}) => {
-    let newStudent = {name, house}
-    this.setState({students: [...this.state.students, newStudent]})
+  addStudent = (newStudent) => {
+    this.setState({students: [...this.state.students, newStudent], filter: 'All'})
   }
 
-  filterHandler = (e) => {
-    this.setState({filter: e.target.value})
-  }
+  filterHandler = (e) => this.setState({filter: e.target.value})
 
   displayStudents = () => {
     let filteredStudents = [...this.state.students]
